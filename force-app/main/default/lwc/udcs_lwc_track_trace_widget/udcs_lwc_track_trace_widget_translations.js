415753d825c83b5e6d948b138bf20c67dc804f70
Francis Raj <francis.raj@udtrucks.co.za>
2025-01-23 11:59:29 +0530

    track trae changes

---
+++ force-app/main/default/lwc/udcs_lwc_track_trace_widget/udcs_lwc_track_trace_widget_translations.js
@@ -0,0 +1,15 @@
+import lbl_ud_map from "@salesforce/label/c.ud_map";
+import lbl_ud_realtimetracking from "@salesforce/label/c.ud_realtimetracking";
+import lbl_ud_Truck from "@salesforce/label/c.ud_Truck";
+import lbl_ud_geofences from "@salesforce/label/c.ud_geofences";
+import lbl_ud_vehicles from "@salesforce/label/c.ud_vehicles";
+import lbl_ud_active from "@salesforce/label/c.ud_active";
+
+export default {
+  lbl_ud_map,
+  lbl_ud_realtimetracking,
+  lbl_ud_Truck,
+  lbl_ud_geofences,
+  lbl_ud_vehicles,
+  lbl_ud_active
+};
